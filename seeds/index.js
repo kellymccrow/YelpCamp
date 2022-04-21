c9bdfce96b308565a52b20102fa733b9ba0f5e0d
Kelly McCrow-Young <kelly.ann.tuesday@gmail.com>
2022-04-21 17:01:59 +1200

    update seeds database

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,13 @@ const nz = require('./nz');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+ 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl)
 .then(() => {
     console.log('Database connected')
 })
